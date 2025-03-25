e03303d1cf30e8bcacac7600074ee17971d26e19
Vadiv <184131687+vadiv24@users.noreply.github.com>
2025-03-25 15:57:21 +0000

    trigger tf-doc

---
+++ examples/deps.tf
@@ -0,0 +1,48 @@
+module "resource_group_a" {
+  source = "github.com/Azure-Terraform/terraform-azurerm-resource-group.git?ref=v2.1.0"
+
+  location = module.metadata.location
+  names = {
+    environment         = "sandbox"
+    location            = "uksouth"
+    market              = "uk"
+    product_name        = "poc2"
+    resource_group_type = "shared"
+  }
+  tags = module.metadata.tags
+}
+
+module "resource_group_b" {
+  source = "github.com/Azure-Terraform/terraform-azurerm-resource-group.git?ref=v2.1.0"
+
+  location = module.metadata.location
+  names = {
+    environment         = "sandbox"
+    location            = "uksouth"
+    market              = "uk"
+    product_name        = "poc1"
+    resource_group_type = "shared"
+  }
+  tags = module.metadata.tags
+}
+
+module "metadata" {
+  source = "github.com/Azure-Terraform/terraform-azurerm-metadata.git?ref=v1.1.0"
+
+  naming_rules = module.naming.yaml
+
+  market              = "us"
+  project             = "https://github.com/Azure-Terraform/terraform-azurerm-storage-account/tree/main/example"
+  location            = "eastus2"
+  environment         = "sandbox"
+  product_name        = "contossoweb"
+  business_unit       = "infra"
+  product_group       = "contoso"
+  subscription_id     = ""
+  subscription_type   = "dev"
+  resource_group_type = "shared"
+}
+
+module "naming" {
+  source = "git@github.com:Azure-Terraform/example-naming-template.git?ref=v1.0.0"
+}
--- examples/main.tf
+++ examples/main.tf
@@ -24,20 +24,6 @@ module "dns_a" {
   }
 }
 
-module "resource_group_a" {
-  source = "github.com/Azure-Terraform/terraform-azurerm-resource-group.git?ref=v2.1.0"
-
-  location = module.metadata.location
-  names = {
-    environment         = "sandbox"
-    location            = "uksouth"
-    market              = "uk"
-    product_name        = "poc2"
-    resource_group_type = "shared"
-  }
-  tags = module.metadata.tags
-}
-
 module "dns_b" {
   source = "../"
 
@@ -51,39 +37,4 @@ module "dns_b" {
     azurerm.parent = azurerm.parent
     azurerm.child  = azurerm.child
   }
-}
-
-module "resource_group_b" {
-  source = "github.com/Azure-Terraform/terraform-azurerm-resource-group.git?ref=v2.1.0"
-
-  location = module.metadata.location
-  names = {
-    environment         = "sandbox"
-    location            = "uksouth"
-    market              = "uk"
-    product_name        = "poc1"
-    resource_group_type = "shared"
-  }
-  tags = module.metadata.tags
-}
-
-module "metadata" {
-  source = "github.com/Azure-Terraform/terraform-azurerm-metadata.git?ref=v1.1.0"
-
-  naming_rules = module.naming.yaml
-
-  market              = "us"
-  project             = "https://github.com/Azure-Terraform/terraform-azurerm-storage-account/tree/main/example"
-  location            = "eastus2"
-  environment         = "sandbox"
-  product_name        = "contossoweb"
-  business_unit       = "infra"
-  product_group       = "contoso"
-  subscription_id     = ""
-  subscription_type   = "dev"
-  resource_group_type = "shared"
-}
-
-module "naming" {
-  source = "git@github.com:Azure-Terraform/example-naming-template.git?ref=v1.0.0"
 }
