d7e1827777e6b26bdba15184a008a133b9a74d1b
Vadiv <184131687+vadiv24@users.noreply.github.com>
2025-03-31 22:21:25 +0100

    revert azurerm version as it was before

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source                = "hashicorp/azurerm"
-      version               = ">= 4.0"
+      version               = ">= 2.0.0"
       configuration_aliases = [azurerm.parent, azurerm.child]
     }
   }
